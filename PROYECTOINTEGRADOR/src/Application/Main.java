cede77ebb956bc8759e89b4e5bd72941aad3137e
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-05 17:02:23 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Application/Main.java
+++ PROYECTOINTEGRADOR/src/Application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MAIN.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MenuRol.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());
 			primaryStage.setScene(scene);
