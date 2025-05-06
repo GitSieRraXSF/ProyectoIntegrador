1fae8e03d1b71c100ff2af5bd70c4355770b6078
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-06 17:41:57 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Application/Main.java
+++ PROYECTOINTEGRADOR/src/Application/Main.java
@@ -3,6 +3,7 @@ package Application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
@@ -24,5 +25,11 @@ public class Main extends Application {
 		launch(args);
 	}
 	
-	
+	public static void showAlert(String title, String header, String message,Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
