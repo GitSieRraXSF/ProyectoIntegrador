ee29d394c162a9f0916f405aee5fe3da47be9023
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-06 17:56:58 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Application/Main.java
+++ PROYECTOINTEGRADOR/src/Application/Main.java
@@ -1,5 +1,5 @@
 package Application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -8,28 +8,40 @@ import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
+	private static AnchorPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("LoginPI.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LoginPI.fxml"));
+			rootLayout = loader.load();
+			Scene scene = new Scene(rootLayout);
+			scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	public static void showAlert(String title, String header, String message,Alert.AlertType type) {
-        Alert alert = new Alert(type);
-        alert.setTitle(title);
-        alert.setHeaderText(header);
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
+
+	public static void loadView(String fxmlFile) {
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlFile));
+			rootLayout.setCenterShape(loader.load());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void showAlert(String title, String header, String message, Alert.AlertType type) {
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(message);
+		alert.showAndWait();
+	}
 }
