ac6bfc7c4b62010f0915fc260117aa280ef34f82
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-06 17:34:35 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Data/EncuestaDAO.java
+++ PROYECTOINTEGRADOR/src/Data/EncuestaDAO.java
@@ -1,5 +1,38 @@
 package Data;
 
-public class EncuestaDAO {
+import java.util.ArrayList;
 
+import Model.Encuesta;
+
+public class EncuestaDAO implements CRUD_PI<Encuesta, String>{
+
+	@Override
+	public void save(Encuesta entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Encuesta> fetch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Encuesta entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean authenticate(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- PROYECTOINTEGRADOR/src/Data/SancionDAO.java
+++ PROYECTOINTEGRADOR/src/Data/SancionDAO.java
@@ -1,5 +1,38 @@
 package Data;
 
-public class SancionDAO {
+import java.util.ArrayList;
 
+import Model.Sancion;
+
+public class SancionDAO implements CRUD_PI<Sancion, String>{
+
+	@Override
+	public void save(Sancion entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Sancion> fetch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Sancion entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean authenticate(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
