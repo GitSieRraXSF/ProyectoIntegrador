de633e636adcd5ba348a95624e23fa7a201fda4b
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-07 17:48:31 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Data/SolicitudPrestamoDAO.java
+++ PROYECTOINTEGRADOR/src/Data/SolicitudPrestamoDAO.java
@@ -1,10 +1,20 @@
 package Data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import Model.SolicitudPrestamo;
 
-public class SolicitudPrestamoDAO implements CRUD_PI<SolicitudPrestamo, String>{
+public class SolicitudPrestamoDAO implements CRUD_PI<SolicitudPrestamo, String> {
+	
+	private Connection connection;
+	
+	public SolicitudPrestamoDAO (Connection connection) {
+		this.connection = connection;
+	}
 
 	@Override
 	public void save(SolicitudPrestamo entity) {
@@ -31,8 +41,17 @@ public class SolicitudPrestamoDAO implements CRUD_PI<SolicitudPrestamo, String>{
 	}
 
 	@Override
-	public boolean authenticate(String id) {
-		// TODO Auto-generated method stub
+	public boolean authenticate(String estado) {
+/*		String sql = "";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setString(1, nickname);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				return rs.getString("nickname").equals(nickname) && rs.getString("contraseña").equals(contraseña);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}*/
 		return false;
 	}
 }
--- PROYECTOINTEGRADOR/src/module-info.java
+++ PROYECTOINTEGRADOR/src/module-info.java
@@ -1,6 +1,7 @@
 module PROYECTOINTEGRADOR {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens Application to javafx.graphics, javafx.fxml;
 }
