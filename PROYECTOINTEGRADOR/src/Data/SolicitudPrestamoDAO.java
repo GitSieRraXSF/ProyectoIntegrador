5e16a5dfbf947469c1cf34c8222b96dcfa1fa834
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-23 17:45:19 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Data/SolicitudPrestamoDAO.java
+++ PROYECTOINTEGRADOR/src/Data/SolicitudPrestamoDAO.java
@@ -1,12 +1,14 @@
 package Data;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
+import Application.Main;
 import Model.SolicitudPrestamo;
+import javafx.scene.control.Alert;
+import oracle.jdbc.internal.OracleTypes;
 
 public class SolicitudPrestamoDAO {
 	
@@ -17,8 +19,8 @@ public class SolicitudPrestamoDAO {
 	}
 	
 	public void save(SolicitudPrestamo solicitud) {
-		String sql = "INSERT INTO Solicitud_Prestamo (fechaSolicitud, fechaDevolucionREAL, HoraInicio, HoraFin, Estado) VALUES (?, ?, ?, ?, ?)";
-		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+		String sql = "{call InsertSolicitudPrestamo(?, ?, ?, ?, ?)}";
+		try (CallableStatement stmt = connection.prepareCall(sql)) {
 			stmt.setString(1, solicitud.getFechaSolicitud());
 			stmt.setString(2, solicitud.getFechaDevolucionReal());
 			stmt.setString(3, solicitud.getFechainicio());
@@ -27,31 +29,37 @@ public class SolicitudPrestamoDAO {
 			stmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			Main.showAlert("Error...!", "Proceso invalido!", e.getMessage(), Alert.AlertType.ERROR);
 		}
 	}
 	
 	public ArrayList<SolicitudPrestamo> fetch() {
 		ArrayList<SolicitudPrestamo> solicitudes = new ArrayList<>();
-		String sql = "SELECT * FROM SolicitudPrestamo";
-		try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
-			while (rs.next()) {
-				String fechaSolicitud = rs.getString("fechaSolicitud");
-				String fechainicio = rs.getString("HoraInicio");
-				String fechafinPrevista = rs.getString("HoraFin");
-				String fechaDevolucionReal = rs.getString("fechaDevolucionReal");
-				boolean estado = rs.getBoolean("Estado");
-				SolicitudPrestamo solicitud = new SolicitudPrestamo(fechaSolicitud, fechainicio, fechafinPrevista, fechaDevolucionReal, estado);
-				solicitudes.add(solicitud);
+		String sql = "{? = call FetchSolicitudPrestamo()}";
+		try (CallableStatement cs = connection.prepareCall(sql)) {
+			cs.registerOutParameter(1, OracleTypes.CURSOR);
+			cs.execute();
+			try (ResultSet rs = (ResultSet) cs.getObject(1)){
+				while (rs.next()) {
+					String FechaSolicitud = rs.getString("FechaSolicitud");
+					String fechainicio = rs.getString("fechainicio");
+					String fechafinPrevista = rs.getString("fechafinPrevista");
+					String fechaDevolucionReal = rs.getString("fechaDevolucionReal");
+					boolean estado = rs.getBoolean(0);
+					SolicitudPrestamo Solicitud = new SolicitudPrestamo(FechaSolicitud, fechainicio, fechafinPrevista, fechaDevolucionReal, estado);
+					solicitudes.add(Solicitud);
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+			Main.showAlert("Error...!", "Proceso invalido!", e.getMessage(), Alert.AlertType.ERROR);
 		}
-		return null;
+		return solicitudes;
 	}
 	
 	public void update(SolicitudPrestamo solicitud) {
-		String sql = "UPDATE Solicitud_Prestamo SET fechaSolicitud=?, fechaDevolucionREAL=?, HoraInicio=?, HoraFin=?, Estado=?";
-		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+		String sql = "{call = UpdateSolicitud(?, ?, ?, ?, ?)}";
+		try (CallableStatement stmt = connection.prepareCall(sql)) {
 			stmt.setString(1, solicitud.getFechaSolicitud());
 			stmt.setString(2, solicitud.getFechaDevolucionReal());
 			stmt.setString(3, solicitud.getFechainicio());
@@ -60,29 +68,32 @@ public class SolicitudPrestamoDAO {
 			stmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			Main.showAlert("Error...!", "Proceso invalido!", e.getMessage(), Alert.AlertType.ERROR);
 		}
 	}
 	
-	public void delete(int IDSolicitud) {
-		String sql = "DELETE FROM Book WHERE IDSolicitud=?";
-		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-			stmt.setLong(1, IDSolicitud);
-			stmt.executeUpdate();
+	public void delete(String fecha) {
+		String sql = "{call DeleteSolicitud(?)}";
+		try (CallableStatement stmt = connection.prepareCall(sql)) {
+			stmt.setString(1, fecha);
+			stmt.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			Main.showAlert("Error...!", "Proceso invalido!", e.getMessage(), Alert.AlertType.ERROR);
 		}
 	}
 	
 	public boolean authenticate(String fecha) {
-		String sql = "SELECT fechaSolicitud FROM Solicitud WHERE fechaSolicitud=?";
-		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-			stmt.setString(1, fecha);
-			ResultSet rs = stmt.executeQuery();
-			if (rs.next()) {
-				return rs.getString("fechasolicitud").equals(fecha);
-			}
+		String sql = "{? = call AuthenticateSolicitud(?)}";
+		try (CallableStatement stmt = connection.prepareCall(sql)) {
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setString(2, fecha);
+			stmt.execute();
+			int result = stmt.getInt(1);
+			return result == 1;
 		} catch (SQLException e) {
 			e.printStackTrace();
+			Main.showAlert("Error...!", "Proceso invalido!", e.getMessage(), Alert.AlertType.ERROR);
 		}
 		return false;
 	}
