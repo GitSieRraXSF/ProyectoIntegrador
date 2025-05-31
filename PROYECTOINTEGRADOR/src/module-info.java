68a28448274faf999269937843638be981e2eb8d
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-05-31 18:31:52 -0500

    Commit

--- PROYECTOINTEGRADOR/src/module-info.java
+++ PROYECTOINTEGRADOR/src/module-info.java
@@ -4,8 +4,6 @@ module PROYECTOINTEGRADOR {
 	requires java.sql;
 	requires javafx.base;
 	requires javafx.graphics;
-	requires poi;
-	requires poi.ooxml;
 	requires com.oracle.database.jdbc;
 	
 	opens Application to javafx.graphics, javafx.fxml;
