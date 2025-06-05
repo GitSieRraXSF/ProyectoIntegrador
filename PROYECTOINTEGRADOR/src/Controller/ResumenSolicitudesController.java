3d53ce3f37922cab457214be6efd189b2c86bfed
Usuario <Usuario@DESKTOP-L01EDQJ>
2025-06-05 13:42:19 -0500

    Commit

--- PROYECTOINTEGRADOR/src/Controller/ResumenSolicitudesController.java
+++ PROYECTOINTEGRADOR/src/Controller/ResumenSolicitudesController.java
@@ -3,15 +3,12 @@ package Controller;
 import java.io.File;
 import java.sql.Connection;
 import java.util.ArrayList;
-
 import Application.Main;
 import Data.ApacheProyect;
 import Data.DBConnectionFactory;
 import Data.SolicitudPrestamoDAO;
 import Model.SolicitudPrestamo;
 import Model.Usersession;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -55,18 +52,19 @@ public class ResumenSolicitudesController {
     @FXML
     public void initialize() {
     	
-    	ObservableList<SolicitudPrestamo> availableSolicitudes = FXCollections.observableArrayList();
-    	for (SolicitudPrestamo solicitud2 : SolicitudDAO.fetch()) {
-    		availableSolicitudes.add(solicitud2);
-    	}
-    	// Bind only the columns you want to show
-    	colNombreuser.setCellValueFactory(new PropertyValueFactory<>("FechaSolicitud"));
-    	colfechainicio.setCellValueFactory(new PropertyValueFactory<>("fechainicio"));
-    	colfechafinprevista.setCellValueFactory(new PropertyValueFactory<>("fechafinPrevista"));
-    	colfechaDevo.setCellValueFactory(new PropertyValueFactory<>("fechaDevolucionReal"));
-    	colEstado.setCellValueFactory(new PropertyValueFactory<>("Estado"));
-    	// Set data to TableView
-    	tableSolicitudes.setItems(availableSolicitudes);
+//    	ObservableList<SolicitudPrestamo> availableSolicitudes = FXCollections.observableArrayList();
+//    	for (SolicitudPrestamo solicitud2 : SolicitudDAO.fetch()) {
+//    		availableSolicitudes.add(solicitud2);
+//    	}
+//    	// Bind only the columns you want to show
+//    	colNombreuser.setCellValueFactory(new PropertyValueFactory<>("FechaSolicitud"));
+//    	colfechainicio.setCellValueFactory(new PropertyValueFactory<>("fechainicio"));
+//    	colfechafinprevista.setCellValueFactory(new PropertyValueFactory<>("fechafinPrevista"));
+//    	colfechaDevo.setCellValueFactory(new PropertyValueFactory<>("fechaDevolucionReal"));
+//    	colEstado.setCellValueFactory(new PropertyValueFactory<>("Estado"));
+//    	// Set data to TableView
+//    	tableSolicitudes.setItems(availableSolicitudes);
+    	LoadTableView(SolicitudDAO.fetch());
     }
 
     @FXML
