42ad05f133098d2a71158b55db77e339f50845b1
ESTUDIANTE <ESTUDIANTE@172.16.0.103>
2025-06-10 08:46:19 -0500

    Commit

--- PROYECTOINTEGRADOR/src/module-info.java
+++ PROYECTOINTEGRADOR/src/module-info.java
@@ -5,6 +5,8 @@ module PROYECTOINTEGRADOR {
 	requires javafx.base;
 	requires javafx.graphics;
 	requires com.oracle.database.jdbc;
+	requires poi;
+	requires poi.ooxml;
 	
 	opens Application to javafx.graphics, javafx.fxml;
 }
